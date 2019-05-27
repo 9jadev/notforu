694f851b97f5337136d5a7569914c3faf32b1cb6
solomon <ahambasolomon800@gmail.com>
2019-05-27 11:05:35 +0100

    just do it

--- didit.php
+++ didit.php
@@ -2,8 +2,8 @@
 require 'vendor/autoload.php';
 use Mailgun\Mailgun;
 # Instantiate the client.
-$mgClient = new Mailgun('f72dd7b55955806962dc3b470480bbb0-52b0ea77-9a23a0ae');
-$domain = "https://api.mailgun.net/v3/app.shbankonline.com";
+$mgClient = new Mailgun('');
+$domain = "";
 # Make the call to the client.
 $result = $mgClient->sendMessage($domain, array(
 	'from'	=> 'Excited User <mailgun@app.shbankonline.com>',
